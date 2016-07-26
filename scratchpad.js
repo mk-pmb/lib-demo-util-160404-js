4972fa92eba73d393c4975f0e21a678b7e5a0439
mk-pmb <mk-pmb@users.noreply.github.com>
2016-07-26 19:11:59 +0200

    add scratchpad

---
+++ scratchpad.js
@@ -0,0 +1,16 @@
+﻿/*jslint indent: 2, maxlen: 80, node: true */
+/* -*- tab-width: 2 -*- */
+'use strict';
+
+var D = require('lib-demo-util-160404')();
+
+D.result = 'Your data';
+D.expect('!==', 'Your expectation');
+
+
+
+
+
+
+
+D.ok(module);     //= "+OK all scratchpad tests passed."
